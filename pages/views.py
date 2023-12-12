ea9c30924358ece292e280808c30901dd5f0797e
tamirgroup.net <tamirgroup.net@tamirgroupnets-MacBook-Pro.local>
2023-12-12 14:32:48 +0330

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+#def home_page_view(request):
+#    return HttpResponse("Hello, World!")
+
+# Create your views here.
