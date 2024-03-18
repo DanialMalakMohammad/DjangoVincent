5f7ed4b3ab1b43cd9a6beabe64376840870f47bd
tamirgroup.net <danial23mm@yahoo.com>
2024-03-18 19:19:48 +0330

    test commit 4

--- pages/urls.py
+++ pages/urls.py
@@ -5,7 +5,7 @@ from .views import HomePageView,AboutPageView
 #from .views import home_page_view
 
 urlpatterns = [
-    path("", home_page_view, name="home"),
-    path("", HomePageView.as_view(), name="home"),
+    #path("", home_page_view, name="home"),
+    #path("", HomePageView.as_view(), name="home"),
     path("about/",AboutPageView.as_view(),name="about")
 ]
